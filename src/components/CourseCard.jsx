36e589326210a50c0e7eea822818417aca1789af
jonahhess <jonahhessdev@gmail.com>
2026-07-02 15:35:53 +0300

    added ? to .title

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -35,8 +35,8 @@ export default function CourseCard({ course, coursewares, onClick, isNew }) {
       {(!isNew && (
         <>
           <Text size="sm" c="dimmed" mb="sm">
-            Lesson {index + 1}:{" "}
-            {currentCW?.title || courseTitles[index + 1].title}
+            Lesson {index + 1} " "
+            {currentCW?.title || courseTitles[index + 1]?.title}
           </Text>
           <Progress value={progress} label={`${progress}%`} />
         </>
