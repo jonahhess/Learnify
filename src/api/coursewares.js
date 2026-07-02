e9773a68fdc5d9499326b524d1349d317a9d4ff5
jonahhess <jonahhessdev@gmail.com>
2026-07-02 11:57:21 +0300

    added startCourseware api

--- src/api/coursewares.js
+++ src/api/coursewares.js
@@ -1,6 +1,15 @@
 import { jsonFetch } from "./auth.js";
 import { API_BASE_URL } from "./baseUrl.js";
 
+export function startCourseware(userId, coursewareId) {
+  return jsonFetch(
+    `${API_BASE_URL}/users/${userId}/coursewares/${coursewareId}`,
+    {
+      method: "POST",
+    },
+  );
+}
+
 export function submitCourseware(userId, coursewareId) {
   return jsonFetch(
     `${API_BASE_URL}/users/${userId}/coursewares/${coursewareId}`,
