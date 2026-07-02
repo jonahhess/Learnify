4bbed69f17fdbd002cc8bd2f00ead1a688678ef0
jonahhess <jonahhessdev@gmail.com>
2026-07-02 15:27:32 +0300

    get title out of object

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -38,7 +38,8 @@ export default function CourseCard({ course, coursewares, onClick, isNew }) {
       {(!isNew && (
         <>
           <Text size="sm" c="dimmed" mb="sm">
-            Lesson {index + 1}: {currentCW?.title || courseTitles[index + 1]}
+            Lesson {index + 1}:{" "}
+            {currentCW?.title || courseTitles[index + 1].title}
           </Text>
           <Progress value={progress} label={`${progress}%`} />
         </>
