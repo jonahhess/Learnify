d88e3bc5c4e18da5451fb3be99c3c5b75ee3659d
jonahhess <jonahhessdev@gmail.com>
2026-07-05 12:27:17 +0300

    removed coursewares prop

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,14 +1,13 @@
 import { SimpleGrid } from "@mantine/core";
 import CourseCard from "./CourseCard.jsx";
 
-export default function CourseList({ courses, coursewares, onSelectCourse, isNew }) {
+export default function CourseList({ courses = [], onSelectCourse, isNew }) {
   return (
     <SimpleGrid cols={{ base: 1, sm: 2, md: 3 }}>
       {courses.map((course) => (
         <CourseCard
           key={course._id}
           course={course}
-          coursewares={coursewares}
           isNew={isNew}
           onClick={() => onSelectCourse(course)}
         />
